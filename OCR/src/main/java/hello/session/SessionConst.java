3ca62eea17226c918586b28681550dd6551bfc52
bangw08 <bangwol08@naver.com>
2023-12-20 20:11:27 +0900

    path change

--- OCR/src/main/java/hello/session/SessionConst.java
+++
@@ -1,6 +0,0 @@
-package hello.session;
-
-public class SessionConst {
-    public static final String LOGIN_MEMBER = "loginMember";
-
-}
---
+++ src/main/java/hello/controller/MemberController.java
@@ -0,0 +1,40 @@
+package hello.controller;
+
+import hello.domain.Member;
+import hello.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.Table;
+
+@Controller
+@RequiredArgsConstructor
+@Table(name = "member")
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "members/addMemberForm";
+        }
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
